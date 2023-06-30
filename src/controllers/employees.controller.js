e187aebc5f54edb11a720b9fcf703496c7ec9356
alejandrorodriguez <alejandrorodriguezjimenez17@outlook.com>
2023-06-30 17:49:33 -0500

    Get employee by id

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -1,6 +1,19 @@
 import { pool } from '../db.js'
 
-export const getEmployees = (req, res) => res.send('Getting employees')
+export const getEmployees = async (req, res) => {
+   const [rows] = await pool.query('SELECT * FROM employee')
+   res.json(rows)
+}
+
+export const getEmployee = async (req, res) => {
+   const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [req.params.id])
+   
+   if (rows.length <= 0) return res.status(404).json({
+      message: "Employee not found."
+   })
+
+   res.json(rows[0])
+}
 
 export const createEmployee = async (req, res) => {
    const { name, salary } = req.body

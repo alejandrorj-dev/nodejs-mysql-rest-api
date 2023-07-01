a3519e00481c53894e76096c74d86ca7a82f405f
alejandrorodriguez <alejandrorodriguezjimenez17@outlook.com>
2023-07-01 18:00:18 -0500

    Delete employee

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -26,6 +26,15 @@ export const createEmployee = async (req, res) => {
    })
 }
 
-export const updateEmployee = (req, res) => res.send('Updating employees')
+export const deleteEmployee = async (req, res) => {
+   const [result] = await pool.query('DELETE FROM employee WHERE id = ?', [req.params.id])
 
-export const deleteEmployee = (req, res) => res.send('Deleting employees')
+   if (result.affectedRows <= 0) return res.status(404).json({
+         message: "Employee not found."
+      })
+   
+
+   res.sendStatus(204)
+}
+
+export const updateEmployee = (req, res) => res.send('Updating employees')
